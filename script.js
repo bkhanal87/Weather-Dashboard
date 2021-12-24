e19ed0d475c7f8ff26ed6df777049408496a1dd9
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-12-23 21:29:46 -0500

    updating latest changes

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function onPageLoad() {
     li.appendChild(button);
     citiesListWrapper.appendChild(li);
   });
-};
+}
 
 onPageLoad();
 
@@ -33,13 +33,14 @@ function apiCall(cityEntered) {
     apikey +
     "&units=" +
     unit;
+
   fetch(weatherUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       let cityEl = document.querySelector("#cityName");
-      cityEl.textContent = cityEntered + ' (' + (currentDate) + ')';
+      cityEl.textContent = cityEntered + " (" + currentDate + ")";
       let tempEl = document.querySelector("#temp");
       tempEl.textContent = "Temperature: " + parseInt(data.main.temp) + "°F";
       let humidityEl = document.querySelector("#humidity");
@@ -76,41 +77,43 @@ function apiCall(cityEntered) {
             uvIndexEl.classList.add("severe");
           }
         });
-      fiveDayAPICall(cityEntered);
+      fiveDayAPICall({ lat, lon });
     });
 }
 
 // To get five-day forecast weather
-function fiveDayAPICall(cityEntered) {
+function fiveDayAPICall({ lat, lon }) {
   let fiveDayAPI =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    cityEntered +
-    "&appid=" +
-    apikey +
-    "&units=" +
-    unit;
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon;
+  "&exclude=" + hourly, daily;
+  "&appid=" + apikey;
+
   fetch(fiveDayAPI)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      //let data = data;
+      // let data = data;
       let arrayList = data.list;
 
       // For each loop to get the data for each day from the array
       arrayList.forEach((dayObject) => {
         let noon = dayObject.dt_txt.includes("12:00:00");
         if (noon) {
-          let date = dayObject.dt_txt.split(' ')[0];
+          // setting variables to put into the document
+          let date = dayObject.dt_txt.split(" ")[0];
           let temp = dayObject.main.temp;
           let humidity = dayObject.main.humidity;
           let icon = dayObject.weather[0].icon;
-          let iconUrl = "" + icon + "@2x.png";
+          let iconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
           let forecast = document.querySelector(".forecastWrapper");
           let dayForecastData = document.createElement("div");
-          dayForecastData.className = "forecastBox";
+          dayForecastData.className = "forecast_box";
 
-          dayForecastData.innerHTML = `<P class = "forecastDate">${date}</p><img src="${iconUrl}"><p class="forecast_temp">Temp:${temp} F</p><p class="forecastHumidity">Humidity:${humidity}%</p>`;
+          dayForecastData.innerHTML = `<P class = "forecast_date">${date}</p> <img src="${iconUrl}"><p class="forecast_temp">Temp:${temp} F</p> <p class="forecast_humidity">Humidity:${humidity}%</p>`;
 
           forecast.appendChild(dayForecastData);
         }
@@ -121,8 +124,8 @@ function fiveDayAPICall(cityEntered) {
 // To clear out the previous 5 day data if another city is searched for
 
 function clear5Day() {
-    let forecast = document.querySelector(".forecastWrapper");
-    forecast.innerHTML = '';
+  let forecast = document.querySelector(".forecastWrapper");
+  forecast.innerHTML = "";
 }
 
 // Onclick event to add to the city list
@@ -131,11 +134,11 @@ $("#searchBtn").on("click", function () {
   event.preventDefault();
   let cityEntered = $("#citySearched").val().toUpperCase().trim();
 
-// Create an Li element if value of city entered is not equal to ""
+  // Create an Li element if value of city entered is not equal to ""
 
-if(cityEntered !=="") {
+  if (cityEntered !== "") {
     console.log("city button is working");
-    citiesListWrapper.innerHTML = '';
+    citiesListWrapper.innerHTML = "";
     searchHisArray.push(cityEntered);
 
     // To add the cities array to local storage
@@ -144,12 +147,12 @@ if(cityEntered !=="") {
     // To create an li and append to the ul for every city in the array
 
     searchHisArray.forEach((city) => {
-        let li = document.createElement("li");
-        li.classList.add("cityListItem");
-        let button = document.createElement("button");
-        button.innerHTML = city;
-        li.appendChild(button);
-        citiesListWrapper.appendChild(li);
+      let li = document.createElement("li");
+      li.classList.add("cityListItem");
+      let button = document.createElement("button");
+      button.innerHTML = city;
+      li.appendChild(button);
+      citiesListWrapper.appendChild(li);
     });
 
     // To clear the 5 day forecast from before when a city is clicked
@@ -157,5 +160,5 @@ if(cityEntered !=="") {
     clear5Day();
 
     apiCall(cityEntered);
-    }
+  }
 });
